b9fdb012571b080ff822ea17ab2dddb40f98ee9c
M-SIGMA <m.phillipshider@gmail.com>
2016-11-08 08:52:41 +0000

    feat(home): define new ‘bar-chart’, ‘histogram-chart’ and ‘line-chart’ along with their relevent data sets

--- src/app/home/home-controller.js
+++ src/app/home/home-controller.js
@@ -3,5 +3,13 @@ angular
   .controller('HomeCtrl', HomeCtrl);
 
 function HomeCtrl($scope) {
-  //TODO: IMPLEMENT SCOPE/VIEW FUNCTIONALITY
+  var d3 = getD3Instance();
+
+  function getD3Instance() {
+    return angular.isDefined($window.d3) && $window.d3 !== null ? $window.d3 : null;
+  }
+
+  $scope.barChartData = d3.csv('/data/Home_Office_Air_Travel_Data_2011.csv');
+  $scope.histogramChartData = d3.csv('/data/2015-04-23-March-spend-over-500-v1.csv');
+  $scope.barChartData = d3.csv('/data/energy_generation_wc_140114.csv');
 }
